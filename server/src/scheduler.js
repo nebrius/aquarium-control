4e504aecf74e0ae28b5179cea7d5c745847706ba
Bryan Hughes <bryan@theoreticalideations.com>
2013-09-09 04:37:29 +0000

    Fleshed out the scheduler to work on UTC/non-localized systems like the raspberry pi

--- server/src/scheduler.js
+++ server/src/scheduler.js
@@ -21,8 +21,9 @@ var request = require('request'),
 	xml2js = require('xml2js'),
 	schedule = require('node-schedule'),
 
+	mode = 'manual',
 
-	currentState = STATE_OFF,
+	dailySchedule,
 
 	STATE_OFF = 'off',
 	STATE_DAY = 'day',
@@ -39,102 +40,83 @@ function log(level, message) {
     });
 }
 
-function getBaseDate() {
-	var currentTime = new Date();
-	return new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate()).getTime();
-}
-
-/**
- * Fetches the time that sunrise and sunset will occur
- *
- * @param  {Function} callback The callback to call once the times have been fetched
- */
 function fetch(callback) {
-	var date = new Date(),
-		config = JSON.parse(process.env.appSettings).timing;
-	request([config.endpoint, config.latitude, config.longitude, date.getDate(), (date.getMonth() + 1),
-			(-(new Date()).getTimezoneOffset() / 60), '0'].join('/'), function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			xml2js.parseString(body, function (err, results) {
-				var sunrise = results.sun.morning[0].sunrise[0],
-					sunset = results.sun.evening[0].sunset[0],
-					parseDate;
-				switch (date.getDay()) {
-					case 0: parseDate = 'Sun'; break;
-					case 1: parseDate = 'Mon'; break;
-					case 2: parseDate = 'Tue'; break;
-					case 3: parseDate = 'Wed'; break;
-					case 4: parseDate = 'Thu'; break;
-					case 5: parseDate = 'Fri'; break;
-					case 6: parseDate = 'Sat'; break;
-				}
-				parseDate += ', ' + date.getDate() + ' ';
-				switch (date.getMonth()) {
-					case 0: parseDate += 'Jan'; break;
-					case 1: parseDate += 'Feb'; break;
-					case 2: parseDate += 'Mar'; break;
-					case 3: parseDate += 'Apr'; break;
-					case 4: parseDate += 'May'; break;
-					case 5: parseDate += 'Jun'; break;
-					case 6: parseDate += 'Jul'; break;
-					case 7: parseDate += 'Aug'; break;
-					case 8: parseDate += 'Sep'; break;
-					case 9: parseDate += 'Oct'; break;
-					case 10: parseDate += 'Nov'; break;
-					case 11: parseDate += 'Dec'; break;
-				}
-				parseDate += ' ' + date.getFullYear() + ' ';
-				callback({
-					sunrise: new Date(parseDate + sunrise).getTime(),
-					sunset: new Date(parseDate + sunset).getTime()
-				});
-			});
-		}
-	});
+    var date = new Date(),
+        config = JSON.parse(process.env.appSettings).timing;
+    request([config.endpoint, config.latitude, config.longitude, date.getDate(), (date.getMonth() + 1),
+            (-(new Date()).getTimezoneOffset() / 60), '0'].join('/'), function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            xml2js.parseString(body, function (err, results) {
+                var sunriseTwilight = results.sun.morning[0].twilight[0].astronomical[0],
+                    sunrise = results.sun.morning[0].sunrise[0],
+                    sunset = results.sun.evening[0].sunset[0],
+                    sunsetTwilight = results.sun.evening[0].twilight[0].astronomical[0],
+                    parseDate;
+                switch (date.getDay()) {
+                    case 0: parseDate = 'Sun'; break;
+                    case 1: parseDate = 'Mon'; break;
+                    case 2: parseDate = 'Tue'; break;
+                    case 3: parseDate = 'Wed'; break;
+                    case 4: parseDate = 'Thu'; break;
+                    case 5: parseDate = 'Fri'; break;
+                    case 6: parseDate = 'Sat'; break;
+                }
+                parseDate += ', ' + date.getDate() + ' ';
+                switch (date.getMonth()) {
+                    case 0: parseDate += 'Jan'; break;
+                    case 1: parseDate += 'Feb'; break;
+                    case 2: parseDate += 'Mar'; break;
+                    case 3: parseDate += 'Apr'; break;
+                    case 4: parseDate += 'May'; break;
+                    case 5: parseDate += 'Jun'; break;
+                    case 6: parseDate += 'Jul'; break;
+                    case 7: parseDate += 'Aug'; break;
+                    case 8: parseDate += 'Sep'; break;
+                    case 9: parseDate += 'Oct'; break;
+                    case 10: parseDate += 'Nov'; break;
+                    case 11: parseDate += 'Dec'; break;
+                }
+                parseDate += ' ' + date.getFullYear() + ' ';
+                callback({
+                    sunriseTwilight: new Date(parseDate + sunriseTwilight).getTime(),
+                    sunrise: new Date(parseDate + sunrise).getTime(),
+                    sunset: new Date(parseDate + sunset).getTime(),
+                    sunsetTwilight: new Date(parseDate + sunsetTwilight).getTime()
+                });
+            });
+        }
+    });
 }
 
-function scheduleNextChange(configuration) {
+function scheduleNextChange() {
+	
+	// if we are in manual mode, short circuit the scheduler
+	if (mode == 'manual') {
+		return;
+	}
+
 	var currentTime = Date.now(),
-		newState,
-		nextState,
 		nextStateChange;
 
-	// Check the current state and make sure it's valid
-	if (currentTime < configuration.sunrise - configuration.nightOnBeforeSunrise) {
-		newState = STATE_OFF;
-		nextState = STATE_NIGHT;
-		nextStateChange = configuration.sunrise - configuration.nightOnBeforeSunrise;
-	} else if (currentTime < configuration.sunrise) {
-		newState = STATE_NIGHT;
-		nextState = STATE_DAY;
-		nextStateChange = configuration.sunrise;
-	} else if (currentTime < configuration.sunset) {
-		newState = STATE_DAY;
-		nextState = STATE_NIGHT;
-		nextStateChange = configuration.sunset;
-	} else if (currentTime < configuration.sunset + configuration.nightOnAfterSunset) {
-		newState = STATE_NIGHT;
-		nextState = STATE_OFF;
-		nextStateChange = configuration.sunset + configuration.nightOnAfterSunset;
-	} else {
-		newState = STATE_OFF;
-		nextState = STATE_OFF;
-		currentTime = new Date();
-		nextStateChange = new Date(getBaseDate() + 1000 * 60 * 60 * 24);
-	}
-	if (newState !== currentState) {
-		process.send({
-			destination: 'broadcast',
-			type: 'lights.set',
-			data: newState
-		});
-		currentState = newState;
-	}
+	// Find the next state to set
+	while((nextStateChange = dailySchedule.shift()).time < currentTime);
 
 	// Schedule the next state change
-	schedule.scheduleJob(new Date(nextStateChange), function () {
+	schedule.scheduleJob(new Date(nextStateChange.time), function () {
+
+		// if we are in manual mode, short circuit the scheduler
+	    if (mode == 'manual') {
+    	    return;
+    	}
+
+		// Break the recursion chain so that the stack doesn't keep growing and growing
 		setTimeout(function () {
-			scheduleNextChange(configuration);
+			process.send({
+				destination: 'broadcast',
+				type: 'lights.set',
+				data: nextStateChange.state
+			});
+			scheduleNextChange();
 		}, 1000);
 	});
 }
@@ -144,32 +126,52 @@ log('info', 'Scheduler started');
 process.on('message', function (message) {
 	var configuration;
 	if (message.type === 'configuration.set') {
+		log('debug', 'Setting configuration: ' + JSON.stringify(message));
 		configuration = message.data;
-		if (configuration.mode === 'dynamic') {
+		mode = configuration.mode;
+		if (mode === 'automatic') {
 			fetch(function (times) {
-				scheduleNextChange({
-					sunrise: times.sunrise + configuration.dynamic.sunriseOffset,
-					sunset: times.sunset + configuration.dynamic.sunsetOffset,
-					nightOnAfterSunset: configuration.dynamic.nightOnAfterSunset * 60000,
-					nightOnBeforeSunrise: configuration.dynamic.nightOnBeforeSunrise * 60000
-				});
-			});
-		} else if (configuration.mode === 'static') {
-			scheduleNextChange({
-				sunrise: getBaseDate() + (configuration.static.sunrise.hour * 60 + configuration.static.sunrise.minute) * 60000,
-				sunset: getBaseDate() + (configuration.static.sunset.hour * 60 + configuration.static.sunset.minute) * 60000,
-				nightOnAfterSunset: configuration.static.nightOnAfterSunset * 60000,
-				nightOnBeforeSunrise: configuration.static.nightOnBeforeSunrise * 60000
-			});
-		} else if (configuration.mode === 'manual') {
-			if (currentState !== configuration.manual) {
+				
+				// Create the daily schedule
+				dailySchedule = [{
+					state: STATE_NIGHT,
+					time: times.sunriseTwilight
+				},{
+                    state: STATE_DAY,
+                    time: times.sunrise
+                },{
+                    state: STATE_NIGHT,
+                    time: times.sunset
+                },{
+                    state: STATE_OFF,
+                    time: times.sunsetTwilight,
+                }];
+
+				// Adjuset the schedule if the international date line slices the times, e.g. sunrise is after sunset because it is te next day in UTC
+				if (times.sunriseTwilight > times.sunrise) {
+					dailySchedule = dailySchedule.slice(1).concat(dailySchedule.slice(0, 1));
+				} else if (times.sunrise > times.sunset) {
+					dailySchedule = dailySchedule.slice(2).concat(dailySchedule.slice(0, 2));
+				} else if (times.sunset > times.sunsetTwilight) {
+					dailySchedule = dailySchedule.slice(3).concat(dailySchedule.slice(0, 3));
+				}
+
+				// Set the current state (in case it's not already set)
 				process.send({
-					destination: 'broadcast',
-					type: 'lights.set',
-					data: configuration.manual
-				});
-				currentState = configuration.manual;
-			}
+                    destination: 'broadcast',
+                    type: 'lights.set',
+                    data: dailySchedule[3].state
+                });
+
+				// Pump the scheduler
+				scheduleNextChange();
+			});
+		} else if (mode === 'manual') {
+			process.send({
+				destination: 'broadcast',
+				type: 'lights.set',
+				data: configuration.manual
+			});
 		} else {
 			throw new Error('Invalid mode "' + configuration.mode + '"');
 		}
