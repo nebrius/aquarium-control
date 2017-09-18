946270928951ba4009d05bcad79c2625d23953e0
Bryan Hughes <bryan@nebri.us>
2017-09-17 19:29:50 -0700

    Stubbed out project

---
+++ ingester/bin/ingester.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+/*
+Copyright (C) 2013-2017 Bryan Hughes <bryan@nebri.us>
+
+Aquarium Control is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Aquarium Control is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Aquarium Control.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+require('../dist/index').run();
