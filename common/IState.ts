4333b3e31ff38ade6f6acab2040f1b15cd900f14
Bryan Hughes <bryan@nebri.us>
2017-09-19 14:41:41 -0700

    Created state interface

---
+++ common/IState.ts
@@ -0,0 +1,24 @@
+/*
+Copyright (C) 2013-2017 Bryan Hughes <bryan@nebri.us>
+
+Aquarium Control is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Aquarium Control is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Aquarium Control.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+export interface IState {
+  currentTime: string; // new Date() parsable string
+  currentTemperature: number; // in Celcius
+  currentState: 'day' | 'night' | 'off';
+  nextTransitionTime: string; // new Date() parsable string
+  nextTransitionState: 'day' | 'night' | 'off';
+}
