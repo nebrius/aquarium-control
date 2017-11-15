549f0d3bc43c28ea59975c4662e29fecf831b468
Bryan Hughes <bryan@nebri.us>
2017-11-15 14:39:49 -0800

    Final device configuration after deployment

--- controller/dist/config.js
+++ controller/dist/config.js
@@ -16,8 +16,8 @@ You should have received a copy of the GNU General Public License
 along with Aquarium Control.  If not, see <http://www.gnu.org/licenses/>.
 */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DAY_PIN = 'GPIO18';
-exports.NIGHT_PIN = 'GPIO25';
+exports.DAY_PIN = 'GPIO27';
+exports.NIGHT_PIN = 'GPIO17';
 exports.TEMPERATURE_UPDATE_RATE = 10000;
 exports.TEMPERATURE_SAMPLE_SIZE = (5 * 60 * 1000) / exports.TEMPERATURE_UPDATE_RATE;
 exports.CONFIG_FILE_PATH = '/var/local/aquarium-control/config.json';
