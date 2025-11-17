303b7b0c7b3985ec729e55381189ff31fcc6924d
Bryan Hughes <bryan@nebri.us>
2025-11-17 07:03:03 -0800

    Implemented tabs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import { type Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
-import { Header } from "@/components/Header.tsx";
 import { ThemeProvider } from "@/components/ThemeProvider.tsx";
 
 const geistSans = Geist({
@@ -37,7 +36,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Header />
           {children}
         </ThemeProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import { Button } from "@/components/ui/Button.tsx";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/Tabs.tsx";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button>Click me</Button>
-    </div>
+    <Tabs defaultValue="lights">
+      <header className="container flex items-center justify-center w-full">
+        <TabsList className="m-4">
+          <TabsTrigger className="text-3xl px-4" value="lights">
+            Lights
+          </TabsTrigger>
+          <TabsTrigger className="text-3xl px-4" value="cleaning">
+            Cleaning
+          </TabsTrigger>
+        </TabsList>
+      </header>
+      <TabsContent value="lights">
+        <p>Account content</p>
+      </TabsContent>
+      <TabsContent value="cleaning">
+        <p>Password content</p>
+      </TabsContent>
+    </Tabs>
   );
 }
